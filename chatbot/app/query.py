d8d19a3979392e3f2d1f673e7b0cd06f6bd5021a
0152neich <duychien25102004@gmail.com>
2025-04-13 02:10:19 +0700

    update: logic chabot service

--- chatbot/app/query.py
+++ chatbot/app/query.py
@@ -1,18 +1,23 @@
 import logging
+from functools import cached_property
 from shared.base import BaseModel
 from shared.base import BaseService
 from shared.settings import Settings
 
 from .indexing import IndexingService
 from domain.retrieval import RetrievalService
+from domain.retrieval import RetrievalInput
 from domain.generation import GenerationService
 from domain.generation import GenerationInput
+from domain.indexing import EmbeddingService
+from domain.indexing import EmbeddingInput
 from infrastructure.qdrant import Qdrant
 
 logger = logging.getLogger(__name__)
 
 class ChatbotInput(BaseModel):
     query: str
+    user_name: str
 
 class ChatbotOutput(BaseModel):
     response: str
@@ -22,19 +27,23 @@ class ChatbotService(BaseService):
 
     @property
     def _get_indexing(self) -> IndexingService:
-        return IndexingService(setting=self.settings)
+        return IndexingService(settings=self.settings)
 
     @property
     def _get_retrieval(self) -> RetrievalService:
-        return RetrievalService(setting=self.settings)
+        return RetrievalService(settings=self.settings)
     
     @property
     def _get_generation(self) -> GenerationService:
-        return GenerationService(setting=self.settings)
+        return GenerationService(settings=self.settings)
 
     @property
     def _get_qdrant(self) -> Qdrant:
-        return Qdrant(setting=self.settings)
+        return Qdrant(settings=self.settings)
+    
+    @cached_property
+    def _get_embedding(self) -> EmbeddingService:
+        return EmbeddingService(settings=self.settings)
     
     def process(self, inputs: ChatbotInput) -> ChatbotOutput:
         """ Generate a response based on the input query.
@@ -46,8 +55,27 @@ class ChatbotService(BaseService):
             ChatbotOutput: Output data containing the generated response.
         """
         try:
-            retrieval_output = self._get_retrieval.process(inputs.query)
+            embedding_query = self._get_embedding.process(
+                EmbeddingInput(
+                    chunk=[],
+                    query=inputs.query
+                )
+            )
+        except Exception as e:
+            logger.error(f"Error generating embeddings: {e}")
+            raise e
+
+        try:
+            retrieval_output = self._get_retrieval.process(
+                RetrievalInput(
+                    dense_query=embedding_query.dense_embeddings[0],
+                    sparse_query=embedding_query.sparse_embeddings,
+                    user_name=inputs.user_name,
+                )
+            )
             logger.info("Information retrieved successfully.")
+            if not retrieval_output.context:
+                return ChatbotOutput(response="Không tìm thấy thông tin CV liên quan. Bạn có muốn hỏi câu khác không?")
         except Exception as e:
             logger.error(f"Error retrieving information: {e}")
             raise e
@@ -56,7 +84,7 @@ class ChatbotService(BaseService):
             generation_output = self._get_generation.process(
                 GenerationInput(
                     query=inputs.query,
-                    chat_history=retrieval_output.context,
+                    chat_history=[],
                     retrieved_info=retrieval_output.context
                 )
             )
@@ -66,4 +94,3 @@ class ChatbotService(BaseService):
             raise e
         
         return ChatbotOutput(response=generation_output.response)
-        
--- chatbot/domain/generation/generation.py
+++ chatbot/domain/generation/generation.py
@@ -1,11 +1,14 @@
+import logging
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import BaseMessage
 from typing import List, Dict, Any
 from shared.base import BaseModel
 from shared.base import BaseService
+from shared.clean_text import TextCleaner
 from shared.settings import Settings
 
+logger = logging.getLogger(__name__)
 class GenerationInput(BaseModel):
     query: str
     chat_history: List[BaseMessage]
@@ -35,23 +38,27 @@ class GenerationService(BaseService):
                 api_key=self.settings.generation.api_key,
             )
             prompt = ChatPromptTemplate.from_messages([
-            ("system", 
-                "Trả lời câu hỏi về CV chỉ dựa trên thông tin trong tài liệu. "
-                "Trả lời ngắn gọn, chính xác. Nếu không có thông tin hoặc thông tin không liên qian, nói 'Không có thông tin trong CV.'"),
+            ("system",
+                "Bạn là ChatbotAI chuyên dụng trả lời câu hỏi về CV dựa trên thông tin trong tài liệu."
+                "Trả lời chính xác và đầy đủ thông tin một cách tự nhiên."
+                "Nếu không có thông tin hoặc thông tin không liên quan, nói 'Không có thông tin trong CV.'"),
             MessagesPlaceholder(variable_name="chat_history"),
             ("human", "{retrieved_info}\nCâu hỏi: {input}")
             ])
-            retrieved_info_str = "\n".join(
-                [f"Content: {doc.get('content', 'N/A')}\nMetadata: {doc.get('metadata', 'N/A')}" 
-                for doc in inputs.retrieved_info]
+            retrieved_info_str = " ".join(
+                [
+                    f"Content: {doc.get('content', 'N/A')}"
+                    for doc in inputs.retrieved_info
+                ]
             )
-
+            logger.info("Retrieved Info:", retrieved_info_str)
             chain = prompt | llm
             response = chain.invoke({
                 "input": inputs.query,
                 "chat_history": inputs.chat_history,
                 "retrieved_info": retrieved_info_str
             })
-            return GenerationOutput(response=response.content)
+            response = TextCleaner().clean_text(response.content)
+            return GenerationOutput(response=response)
         except Exception as e:
             return GenerationOutput(response=f"Error: {str(e)}")
